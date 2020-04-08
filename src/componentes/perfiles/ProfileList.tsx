6ca446df53ef4ea99774e3fc3c5f5199a0531a66
Santiago Felipe Cerón Araujo - Ceiba Software <santiago.ceron@ceiba.com.co>
2020-04-08 18:30:58 -0500

    click

--- src/componentes/perfiles/ProfileList.tsx
+++ src/componentes/perfiles/ProfileList.tsx
@@ -2,10 +2,19 @@ import * as React from 'react';
 import { ProfilesRepository } from '../../api/profiles.repository';
 import { Profile } from './modelo/profile';
 import './profile-list.css';
+import { RouteComponentProps } from 'react-router-dom';
 
+interface Props extends RouteComponentProps {
+}
 
-export default class ProfileList extends React.Component {
-  state = { perfiles: [] };
+export default class ProfileList extends React.Component<Props, any> {
+
+  
+  constructor(props: Props) {
+    super(props);
+    this.state = { perfiles: [] };
+  }
+
 
   componentDidMount() {
     ProfilesRepository.getAll().then((response) => {
@@ -18,19 +27,19 @@ export default class ProfileList extends React.Component {
     return (
       <div className="card-columns card-container"> {
         this.state.perfiles.map(
-          ({ photo, name, description }: Profile) => {
+          ({ photo, name, description,id }: Profile) => {
 
             photo = photo ? `data:image/jpeg;base64,${photo}` :
               "https://movecopenhagen.com/RegistrationForms/public/export/2019/images/teacher-217.png";
 
             return (
-              <div className="card card-profile-list">
+              <div className="card card-profile-list" key={id}>
                 <img className="card-img-top" src={photo} alt="User without Photo"
                   style={{ width: '200px', height: '200px' }} />
                 <div className="card-body">
                   <h4 className="card-title">{name}</h4>
                   <p className="card-text">{description}</p>
-                  <a href="#" className="btn btn-primary">Update Profile</a>
+                  <a href={`profile/add/${id}`} className="btn btn-primary">Update Profile</a>
                 </div>
               </div>
             );
